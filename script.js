e6abfdbfd958e7c090682e27350eade01c911e16
heni2se <justineamielbc@gmail.com>
2026-05-30 06:27:07 +0800

    started script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        question: "What is the capital of Japan?",
+        choices: ["Beijing", "Seoul", "Tokyo", "Bangkok"],
+        answer: 2
+    },
+    {
+        question: "Which planet is known as the Red Planet?",
+        choices: ["Venus", "Mars", "Jupiter", "Saturn"],
+        answer: 1
+    },
+    {
+        question: "How many sides does a hexagon have?",
+        choices: ["5", "7", "8", "6"],
+        answer: 3
+    },
+    {
+        question: "What does HTML stand for?",
+        choices: ["HyperText Markup Language", "HighText Machine Language", "HyperTool Multi Language", "Hyper Transfer Markup Logic"],
+        answer: 0
+    },
+    {
+        question: "Which of these is a programming language?",
+        choices: ["HTML", "CSS", "Photoshop", "Python"],
+        answer: 3
+    },
+]
