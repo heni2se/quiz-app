fa87f5eea58ddb37e51c68c439ff1f2bd7d2b47d
heni2se <justineamielbc@gmail.com>
2026-05-30 17:27:25 +0800

    added consts and questions. ongoing first function

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
-const questions = [
+const quizContainer = document.getElementById("quiz-container")
+const questionCounter = document.getElementById("question-counter")
+const progressFill = document.getElementById("progress-fill")
+const questionBox = document.getElementById("question-box")
+const questionText = document.getElementById("question-text")
+const optionsBox = document.getElementById("options-box")
+const scoreDisplay = document.getElementById("score-display")
+const nextButton = document.getElementById("next-btn")
+const resultsScreen = document.getElementById("results-screen")
+const finalScore = document.getElementById("final-score")
+const restartButton = document.getElementById("restart-btn")
+
+const questionsList = [
     {
-        question: "What is the capital of Japan?",
-        choices: ["Beijing", "Seoul", "Tokyo", "Bangkok"],
+        question: "1 + 1",
+        choices: ["Burger", "Hotdog", 2, 67],
         answer: 2
     },
     {
-        question: "Which planet is known as the Red Planet?",
-        choices: ["Venus", "Mars", "Jupiter", "Saturn"],
+        question: "does rock beat paper?",
+        choices: ["yes", "no", "Jose Manalo", "Naruto Uzumaki"],
         answer: 1
     },
     {
-        question: "How many sides does a hexagon have?",
-        choices: ["5", "7", "8", "6"],
-        answer: 3
-    },
-    {
-        question: "What does HTML stand for?",
-        choices: ["HyperText Markup Language", "HighText Machine Language", "HyperTool Multi Language", "Hyper Transfer Markup Logic"],
+        question: "is this text a string or an int?",
+        choices: ["sure", "probably", "i miss u baby", "cheesedog"],
         answer: 0
     },
     {
-        question: "Which of these is a programming language?",
-        choices: ["HTML", "CSS", "Photoshop", "Python"],
-        answer: 3
+        question: "are mermaids real?",
+        choices: ["idgaf about mermaids tf u mean", "no r u stupid", "ye", "67"],
+        answer: 1
     },
+    {
+        question: "siomai siopao ____ ?",
+        choices: ["pares", "bagnet", "xiaolongbao", "suman"],
+        answer: 3
+    }
 ]
+
+let currentQuestion = 0
+let score = 0
+let answered = false
+
+function loadQuestion() {
+    answered = false
+    nextButton.disabled = true
+    const current = questionsList[currentQuestion] // takes vvhole objects or questions in the list
+    questionText.textContent = current.question // changes the content in HTML to the current q
+    questionCounter.textContent = (`Question ${currentQuestion + 1} of ${questionsList.length}`) updates the q number
+}
